df194ac941f101bc190d13932d64207a63145375
Juan de la Oliva <juandelaoliva1@gmail.com>
2023-11-14 10:17:36 +0000

    Change input for Challenge 1 and add documentation

--- challenge01/index.php
+++ challenge01/index.php
@@ -1,6 +1,19 @@
+/**
+* FILEPATH: /workspaces/codember2023/challenge01/index.php
+*
+* This script reads a message from a file, splits it into words based on space delimiter,
+* and counts the occurrence of each word. The final result is a string that contains each
+* unique word and its count. The result is printed to the screen.
+*/
 <?php
-// Message from message_01.txt
-$message = "murcielago leon jirafa cebra elefante rinoceronte hipopotamo ardilla mapache zorro lobo oso puma jaguar tigre leopardo gato perro caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco tigre jaguar leopardo oso lobo zorro mapache ardilla elefante rinoceronte hipopotamo cebra jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago tucan loro canario colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago flamenco pinguino tucan loro canario colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo leon jirafa murcielago caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo leon jirafa murcielago caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco murcielago leon jirafa cebra elefante rinoceronte hipopotamo ardilla mapache zorro lobo oso puma jaguar tigre leopardo gato perro caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo jirafa leon murcielago pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon paloma colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon paloma colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon";
+// Read the message from the file
+$message = file_get_contents('message_01.txt');
+
+// Check if the file was read successfully
+if ($message === false) {
+    // Handle the error
+    echo "Error reading file";
+}
 
 // Splitting the message into words based on space delimiter
 $words = explode(" ", $message);
@@ -11,28 +24,28 @@ $finalResult = '';
 
 // Iterating over each word in the message
 foreach ($words as &$word) {
-	// Converting word to lowercase for case-insensitive comparison
+    // Converting word to lowercase for case-insensitive comparison
     $newWord = strtolower($word);
     // Checking if the word already exists in the array
-    if(array_key_exists($newWord, $indexedWords)) {
-    	// Incrementing the count of the word
-    	$indexedWords[$newWord] += 1;
+    if (array_key_exists($newWord, $indexedWords)) {
+        // Incrementing the count of the word
+        $indexedWords[$newWord] += 1;
     } else {
-    	// Adding the word to the array with a count of 1
+        // Adding the word to the array with a count of 1
         $indexedWords[$newWord] = 1;
     }
 }
 
 // Iterating over the indexedWords array to build the final result string
 foreach ($indexedWords as $key => $value) {
-	// Appending the word and its count to the final result with a space for readability
-	$finalResult .= $key . $value;
+    // Appending the word and its count to the final result with a space for readability
+    $finalResult .= $key . $value;
 }
 
 // Print array for visibility.
 print_r($indexedWords);
 
 //This is the result.
-echo $finalResult;
+echo 'Final result--->' . $finalResult;
 
 
